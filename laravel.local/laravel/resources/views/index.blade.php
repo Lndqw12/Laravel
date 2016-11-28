299f77e02fe1237f8bde50646ac86a6d333303c0
Lndqw12 <COMP222012016@mail.ru>
2016-11-28 16:00:55 +0300

    com3

--- laravel.local/laravel/resources/views/index.blade.php
+++ laravel.local/laravel/resources/views/index.blade.php
@@ -1,1 +1,15 @@
-Тюлени
+@extends('app')
+
+@section('content')
+    @foreach($posts as $post)
+        <article>
+            <h2>{{ $post->title }}</h2>
+            <p>
+                {{ $post->excerpt }}
+            </p>
+            <p>
+                  published: {{ $post->publiched_at }}
+            </p>
+        </article>
+    @endforeach
+@stop
